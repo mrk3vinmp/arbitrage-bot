24a23bd0cd6c34e2955db396263f39e0485b8a28
mrk3vinmp <142857362+mrk3vinmp@users.noreply.github.com>
2023-08-21 19:34:33 -0700

    Update Arbitrage.sol

--- contracts/Arbitrage.sol
+++ contracts/Arbitrage.sol
@@ -1,6 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
+interface IERC20 {
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    // Add other ERC20 functions as needed.
+}
+
+
 contract Arbitrage {
     struct Swap {
         address tokenIn;
